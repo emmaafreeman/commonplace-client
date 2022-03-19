ea6ee8fc92acb1542fefd1f4d90de15a4837e958
Emma Vogelmeier <emmameiervogel@gmail.com>
2022-03-19 13:22:09 -0500

    entry search

--- src/components/entry/EntrySearch.js
+++ src/components/entry/EntrySearch.js
@@ -18,15 +18,15 @@ export const EntrySearch = () => {
     getEntries()
   }, [])
 
-  // // Establishes search results as value of search bar and prevents user from entering blank search
-  // const searchResults = (event) => {
-  //   event.preventDefault()
-  //   if (event.target[0].value !== "") {
-  //   getEntry(event.target[0].value)
-  //   } else {
-  //     window.alert("Please enter a movie title")
-  //   }
-  // }
+  // Establishes search results as value of search bar and prevents user from entering blank search
+  const searchResults = (event) => {
+    event.preventDefault()
+    if (event.target[0].value !== "") {
+    getEntries(event.target[0].value)
+    } else {
+      window.alert("Please enter a search term")
+    }
+  }
 
   // // Gets specific movie from TMDb API by id and changes page to Add Movie Form
   // const addMovie = (event) => {
@@ -47,10 +47,10 @@ export const EntrySearch = () => {
           }
         </ul> */}
       <Container className="text-center">
-        <h1 className="page_title">Movie Logger</h1>
-        <Form className="search_form text-center" onSubmit={searchResults}>
+        <h1 className="page_title"></h1>
+        <Form className="search_form text-center" onSubmit={searchResults} >
           <Form.Group className="mb-3">
-            <Form.Control type="text" placeholder="Movie Search" />
+            <Form.Control type="text" placeholder="Entry Search" />
           </Form.Group>
           <Button className="search_button" variant="dark" type="submit">
             Submit
@@ -59,12 +59,10 @@ export const EntrySearch = () => {
       </Container>
       <div className="search_list">
         {
-          apiMovies.map(apiMovie =>
-            <Card className="search_card" style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={ `https://image.tmdb.org/t/p/original/${apiMovie.poster_path}`}/>
+          entries.map(entry =>
+            <Card key={entry.id} className="search_card" style={{ width: '18rem' }}>
               <Card.Body className="text-center">
-                <Card.Title>{apiMovie.original_title}</Card.Title>
-                <Button id={apiMovie.id} onClick={addMovie} variant="dark">Add New Movie</Button>
+                <Link className="text-black" to={`/entries/detail/${entry.id}`}>{entry.title}</Link>
               </Card.Body>
             </Card>
             )
